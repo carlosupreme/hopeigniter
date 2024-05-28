083edf1b3975667de7620749d999565579b12658
Angel Jesus Zorrilla Cuevas <whoangel.agl@gmail.com>
2024-05-28 12:20:09 -0600

    Equipo crud

--- controllers/team.service.js
+++ controllers/team.service.js
@@ -9,6 +9,39 @@ class TeamService {
 		return team;
 	}
 
+	async findById(id) {
+		const team = await Team.findById(id);
+
+		if (!team) throw boom.notFound();
+
+		return team;
+	}
+
+	async updateById(id, data) {
+		const team = await Team.findById(id);
+
+		if (!team) throw boom.notFound();
+
+		for (const field in data) {
+			if (data[field] !== undefined) {
+				if (field === "representative") {
+					// remove if new representative is present
+					team.members = team.members.filter((userId) => {
+						userId !== data[field];
+					});
+
+					//move representative to members
+					team.members.push(team.representative);
+				}
+
+				team[field] = data[field];
+			}
+		}
+
+		await team.save();
+		return team;
+	}
+
 	async find() {
 		try {
 			return await Team.find();
@@ -16,8 +49,6 @@ class TeamService {
 			throw boom.badImplementation();
 		}
 	}
-
-
 }
 
 module.exports = TeamService;
